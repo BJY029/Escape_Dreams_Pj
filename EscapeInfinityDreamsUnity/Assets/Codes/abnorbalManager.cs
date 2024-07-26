99699a6a0a15c7509730b90bdad1c096c1ccebcc
BJY029 <69570614+BJY029@users.noreply.github.com>
2024-07-26 19:49:10 +0900

    Update Text Change
    종이 텍스트가 변경되는 이상현상 추가

--- EscapeInfinityDreamsUnity/Assets/Codes/abnorbalManager.cs
+++ EscapeInfinityDreamsUnity/Assets/Codes/abnorbalManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class abnorbalManager : MonoBehaviour
 {
     public List<GameObject> abnormals; //이상현상용 오브젝트 리스트
     public List<GameObject> originals; //오리지날 오브젝트 리스트
-
+	
 	//초기화 함수
 	public void Init()
-	{ 
+	{
+		//텍스트 할당
+		Text PaperText = abnormals[17].GetComponent<Text>();
 		//이상현상 오브젝트 리스트을 돌아보며
 		for (int i = 0; i < originals.Count; i++)
 		{
@@ -27,7 +30,12 @@ public class abnorbalManager : MonoBehaviour
 			}
 		}
 		abnormals[14].SetActive(false) ;	//새로운 밴트 삭제
-		abnormals[15].SetActive(true) ;		//기존 벤트 활성화
+		abnormals[15].SetActive(true) ;     //기존 벤트 활성화
+		abnormals[16].SetActive(true);      //포스터 활성화
+
+		//텍스트 초기화 작업
+		PaperText.text = "우리 병원은.... 환자들의 건강을 최우선으로 생각합니다.\n우리 병원은.... 최고의 시설을 자랑합니다.\n우리 병원은.... 웃음으로 가득 차 있습니다.";
+		PaperText.color = Color.black;
 	}
 
 	//다음 스테이지 이동 함수
@@ -63,6 +71,18 @@ public class abnorbalManager : MonoBehaviour
 		{
 			abnormals[ranIdx].SetActive(false);
 			Debug.Log("Vent has been unactived");
+		}
+		else if (ranIdx == 16) //사물함 위 새로운 벤트 생성 현상
+		{
+			abnormals[ranIdx].SetActive(false);
+			Debug.Log("Posters has been unactived");
+		}
+		else if(ranIdx == 17) //종이 텍스트 바뀌는 현상
+		{
+			Text PaperText = abnormals[17].GetComponent<Text>();
+			PaperText.text = "정신나갈것같아정신나갈것같아정신나갈것같아정신나갈것같아정신나갈것같아정신나갈것같아정신나갈것같아정신나갈것같아정신나갈것같아정신나갈것같아";
+			PaperText.color = Color.red;
+			Debug.Log("Paper Text has been changed");
 		}
 	}
 }
