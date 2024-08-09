5e6d511c9bd2c6c1bd6fb1d0470e67dabbb13882
DG1004 <ydh05300@gmail.com>
2024-08-09 09:15:02 +0900

    enemy animation update
    복도에 있는 enemy에게 달리기 모션을 적용했습니다.

--- EscapeInfinityDreamsUnity/Assets/Codes/enemy.cs
+++ EscapeInfinityDreamsUnity/Assets/Codes/enemy.cs
@@ -10,11 +10,15 @@ public class enemy : MonoBehaviour
     public Vector3 initialPosition; // 초기 위치 저장 변수
 
     private SpriteRenderer spriteRenderer; // SpriteRenderer 컴포넌트
+    private Animator animator; // Animator 컴포넌트
+    private playerAnimationController playerAnimationController; // 플레이어 애니메이션 컨트롤러
 
     private void Awake()
     {
-        // SpriteRenderer 컴포넌트 가져오기
+        // SpriteRenderer 컴포넌트와 Animator 컴포넌트 가져오기
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        playerAnimationController = player.GetComponent<playerAnimationController>();
     }
     private void OnEnable()
     {
@@ -25,9 +29,17 @@ public class enemy : MonoBehaviour
             player = GameManager.Instance.player.transform;
         }
         spriteRenderer.flipX = true;  //활성화할 때, 오른쪽을 보고있게 좌우반전을 한다.
+        animator.SetBool("IsRunning", false); // 초기 상태는 달리지 않음
     }
     private void FixedUpdate()
     {
+        if (playerAnimationController.playerDeadCoroutine == true)
+        {
+            // 플레이어가 사망 상태라면 적을 멈춤
+            animator.SetBool("IsRunning", false); // 달리기 애니메이션 멈춤
+            return;
+        }
+
         Vector2 direction = player.position - transform.position;
         float distanceToPlayer = direction.magnitude; //플레이어와 적의 거리
 
@@ -37,6 +49,7 @@ public class enemy : MonoBehaviour
 
             Vector2 moveDir = direction.normalized * speed * Time.deltaTime;
             transform.position = (Vector2)transform.position + moveDir;
+            animator.SetBool("IsRunning", true); // 달리기 애니메이션 시작
         }
     }
 }
