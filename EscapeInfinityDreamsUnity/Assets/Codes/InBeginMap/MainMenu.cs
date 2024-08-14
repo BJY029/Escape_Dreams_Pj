605010d4d9e02b83daa4cad609c1ad8e355dc047
Reco <reco27172@gmail.com>
2024-08-14 16:07:48 +0900

    MainScreen
    메인화면 기능 구현
    
    +배경 사운드 대신에 문 잠긴 사운드를 넣어둠. (배경사운드 필요)
    +비주얼 업데이트 필요(도트 작업 할 예정)

---
+++ EscapeInfinityDreamsUnity/Assets/Codes/InBeginMap/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private void Start() // 시작하면 
+    {
+        Time.timeScale = 0f; //모두 멈춤
+    }
+
+    public void GameStart() //시작버튼 클릭시
+    {
+        Time.timeScale = 1.0f; // 원상 복구
+
+    }
+
+    public void GameQuit() //종료버튼 클릭시
+    {
+    #if UNITY_EDITOR //유니티 화면에서 종료
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else //일반 실행시에서 종료
+                Application.Quit();
+    #endif
+    }
+}
