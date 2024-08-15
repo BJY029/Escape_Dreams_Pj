7712eb267a9371550b5a07fe6d24d66c8c770e91
DG1004 <ydh05300@gmail.com>
2024-08-16 06:03:19 +0900

    begin ui update
    시작 씬에서 병원으로 가는 상호작용 ui 구현, 대화창 크기 조정 완료했습니다.
    시작 씬 대화창에 플레이어 아이콘 추가했습니다.

--- EscapeInfinityDreamsUnity/Assets/Codes/InBeginMap/transfer.cs
+++ EscapeInfinityDreamsUnity/Assets/Codes/InBeginMap/transfer.cs
@@ -5,9 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class transfer : MonoBehaviour
 {
+    public GameObject targetObj;//플레이어
     public string MainScene; // 이동할 씬의 이름
     private bool canMoveScene; //씬의 이동 가능 여부(오브젝트와 충돌한 상태인지)
+    public GameObject interactionUI; // 배경을 포함한 UI 오브젝트(text_background)
+    public Vector3 uiOffset; // UI 오브젝트의 출력 위치 조정
 
+    private void Start()
+    {
+        interactionUI.SetActive(false); //시작할 때, 텍스트를 숨김
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,9 +22,13 @@ public class transfer : MonoBehaviour
         //E 버튼이 눌려있고, canMoveScene이 true면 씬을 전환한다.
         if (Input.GetKeyDown(KeyCode.E) && canMoveScene == true)
         {
-            Debug.Log("Move");
+            interactionUI.SetActive(false); // 텍스트를 숨김
             SceneManager.LoadScene(MainScene);
             canMoveScene = false;
+        }
+        if (canMoveScene == true)
+        {
+            interactionUI.transform.position = targetObj.transform.position + uiOffset;
         }
     }
 
@@ -26,8 +37,9 @@ public class transfer : MonoBehaviour
         //플레이어가 오브젝트와 충돌하면 canMoveScene을 true로 설정한다.
         if (collision.CompareTag("Player"))  
         {
-            Debug.Log("Enter");
+            targetObj = collision.gameObject;//플레이어의 게임 오브젝트를 받음
             canMoveScene = true;
+            interactionUI.SetActive(true); // 텍스트를 보이게 함
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -35,8 +47,8 @@ public class transfer : MonoBehaviour
         //플레이어와 오브젝트의 충돌이 끝나면 canMoveScene을 false로 설정한다.
         if(collision.CompareTag("Player"))
         {
-            Debug.Log("exit");
             canMoveScene = false;
+            interactionUI.SetActive(false); // 텍스트를 숨김
         }
     }
 }
